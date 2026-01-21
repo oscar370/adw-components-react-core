6349c779101292fd9679a2cd695c8b403e7ce0d0
oscar370 <oscar.dga@outlook.com>
2026-01-21 16:34:31 -0600

    Refactor ListBox component to simplify header rendering logic

--- components/ui/list-box/list-box.tsx
+++ components/ui/list-box/list-box.tsx
@@ -23,18 +23,25 @@ export function ListBox({
   children,
 }: ListBoxProps) {
   const isSection = Component === "section";
+  const shouldRenderHeader =
+    title || description || headerButton ? true : false;
+
   return (
     <Component className="mt-4">
-      <header className="flex items-center px-px">
-        <div className="flex-1">
-          {title && <h2 className="px-1 font-bold tracking-wider">{title}</h2>}
+      {shouldRenderHeader && (
+        <header className="flex items-center px-px">
+          <div className="flex-1">
+            {title && (
+              <h2 className="px-1 font-bold tracking-wider">{title}</h2>
+            )}
 
-          {description && (
-            <p className="mb-1 px-1 text-xs text-(--dim-fg)">{description}</p>
-          )}
-        </div>
-        {headerButton}
-      </header>
+            {description && (
+              <p className="mb-1 px-1 text-xs text-(--dim-fg)">{description}</p>
+            )}
+          </div>
+          {headerButton}
+        </header>
+      )}
 
       <ul
         className={
