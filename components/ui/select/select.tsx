d9b2f950c26da8dea4dd1454951d0e76ba773864
oscar370 <oscar.dga@outlook.com>
2026-01-22 20:56:36 -0600

    Fix container of select

--- components/ui/select/select.tsx
+++ components/ui/select/select.tsx
@@ -28,34 +28,36 @@ export function Select<T extends string | number>({
 
   return (
     <Listbox value={value} onChange={onChange}>
-      <ListboxButton className="flex w-full cursor-pointer flex-col gap-0.5 transition-colors">
-        <p className="text-start">{title}</p>
+      <div className="relative">
+        <ListboxButton className="flex w-full cursor-pointer flex-col gap-0.5 transition-colors">
+          <p className="text-start">{title}</p>
 
-        <div className="relative h-full w-full rounded-md border-none bg-(--card-bg) p-2 outline-none placeholder:text-(--dim-fg) placeholder:opacity-80 focus:opacity-100">
-          <div className="absolute inset-0 transition-colors hover:bg-(--hover)"></div>
+          <div className="relative h-full w-full rounded-md border-none bg-(--card-bg) p-2 outline-none placeholder:text-(--dim-fg) placeholder:opacity-80 focus:opacity-100">
+            <div className="absolute inset-0 transition-colors hover:bg-(--hover)"></div>
 
-          <span className="flex h-full items-center justify-start gap-1">
-            {selectedLabel}
-            <ChevronDown size={16} />
-          </span>
-        </div>
-      </ListboxButton>
-
-      <ListboxOptions className="mt-1 min-w-50 rounded-xl bg-(--dialog-bg) p-1 shadow-sm focus:outline-none">
-        {options.map((option) => (
-          <ListboxOption
-            key={option.value}
-            value={option.value}
-            className="group flex cursor-pointer items-center rounded-lg px-3 py-2.5 data-focus:bg-(--hover)"
-          >
-            <span className="block truncate">{option.label}</span>
-
-            <span className="ml-auto hidden group-data-selected:block">
-              <Check size={16} />
+            <span className="flex h-full items-center justify-start gap-1">
+              {selectedLabel}
+              <ChevronDown size={16} />
             </span>
-          </ListboxOption>
-        ))}
-      </ListboxOptions>
+          </div>
+        </ListboxButton>
+
+        <ListboxOptions className="mt-1 min-w-50 rounded-xl bg-(--dialog-bg) p-1 shadow-md focus:outline-none">
+          {options.map((option) => (
+            <ListboxOption
+              key={option.value}
+              value={option.value}
+              className="group flex cursor-pointer items-center rounded-lg px-3 py-2.5 data-focus:bg-(--hover)"
+            >
+              <span className="block truncate">{option.label}</span>
+
+              <span className="ml-auto hidden group-data-selected:block">
+                <Check size={16} />
+              </span>
+            </ListboxOption>
+          ))}
+        </ListboxOptions>
+      </div>
     </Listbox>
   );
 }
