285bc3173168030f2a066c2cf97f4d61fd8e1f15
oscar370 <oscar.dga@outlook.com>
2026-01-29 11:07:21 -0600

    The EntrySearch icon was adjusted so that its height matches other Entrys.

--- components/ui/entry-search/entry-search.tsx
+++ components/ui/entry-search/entry-search.tsx
@@ -7,7 +7,7 @@ export function EntrySearch({ type = "search", ...props }: EntrySearchProps) {
   return (
     <div className="w-full overflow-hidden rounded-md transition-colors hover:bg-(--hover)">
       <div className="flex h-full w-full items-center gap-2 bg-(--card-bg) p-2">
-        <Search />
+        <Search size={16} />
 
         <input
           className="h-full w-full border-none outline-none placeholder:text-(--dim-fg) placeholder:opacity-80 focus:opacity-100"
