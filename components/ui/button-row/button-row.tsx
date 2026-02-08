2ece0e8e36ed3bc3dd62b7d5e6440e8a2d961bcd
oscar370 <oscar.dga@outlook.com>
2026-02-08 17:42:42 -0600

    - Fixed the outline in the styles of some components

--- components/ui/button-row/button-row.tsx
+++ components/ui/button-row/button-row.tsx
@@ -14,7 +14,7 @@ export function ButtonRow({
   ...props
 }: ButtonRowProps) {
   const buttonClasses = clsx(
-    "group relative flex min-h-12 w-full cursor-pointer items-center justify-center gap-1 px-4.25 disabled:cursor-not-allowed disabled:opacity-60",
+    "group relative flex min-h-12 w-full cursor-pointer items-center justify-center gap-1 px-4.25 focus:outline-none disabled:cursor-not-allowed disabled:opacity-60",
     variant === "suggested" && "bg-(--accent) text-white",
     variant === "destructive" && "bg-(--destructive) text-white",
     className,
